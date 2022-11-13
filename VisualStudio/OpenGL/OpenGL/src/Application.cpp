92820315cac613c90af3f4a9f865551c5c818095
AndrejLapin <lapin.andrej1@gmail.com>
2022-11-13 21:32:13 +0200

    implemented glm and imgui

--- VisualStudio/OpenGL/OpenGL/src/Application.cpp
+++ VisualStudio/OpenGL/OpenGL/src/Application.cpp
@@ -15,6 +15,13 @@
 #include "Shader.h"
 #include "Texture.h"
 
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
 int main(void)
 {
     GLFWwindow* window;
@@ -27,8 +34,15 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);*/
 
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    const int Width = 640;
+    const int Height = 480;
+    const float Aspect = (float)Width / Height;
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(Width, Height, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -38,7 +52,7 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    glfwSwapInterval(1);
+    glfwSwapInterval(1); // Enable vsync. Sometimes vsync can be enabled by default, to disable pass 0 as parameter.
 
     if (glewInit() != GLEW_OK)
     {
@@ -73,9 +87,12 @@ int main(void)
 
         IndexBuffer ib(indecies, 6);
 
-        Shader shader("res/shaders/Texture_x_Color.shader");
+        glm::mat4 proj = glm::ortho(-1.0f * Aspect, 1.0f * Aspect, -1.0f, 1.0f, -1.0f, 1.0f);
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-0.5f, 0, 0));
+
+        //Shader shader("res/shaders/Texture_x_Color.shader");
         //Shader shader("res/shaders/Texture+Color.shader");
-        //Shader shader("res/shaders/Texture.shader");
+        Shader shader("res/shaders/Texture.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
 
@@ -91,6 +108,24 @@ int main(void)
 
         Renderer renderer;
 
+        // Setup Dear ImGui context
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+        // Setup Dear ImGui style
+        ImGui::StyleColorsDark();
+
+        // Setup Platform/Renderer backends
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init(glsl_version);
+
+        glm::vec3 translation(0.25f, 0.5f, 0);
+
+        // Our state
+        bool show_demo_window = true;
+        bool show_another_window = false;
+        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
         float r = 0.0f;
         float rIncrement = 0.05f;
         float g = 0.5f;
@@ -103,8 +138,17 @@ int main(void)
             /* Render here */
             renderer.Clear();
 
+            // Start the Dear ImGui frame
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+            glm::mat4 mvp = proj * view * model;
+
             shader.Bind();
             shader.SetUniform4f("u_Color", r, g, b, 1.0f);
+            shader.SetUniformMat4f("u_MVP", mvp);
 
             renderer.Draw(va, ib, shader);
 
@@ -116,6 +160,18 @@ int main(void)
             g += gIncrement;
             b += bIncrement;
 
+            // Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+            {
+                ImGui::Begin("ImGUI");
+                ImGui::SliderFloat3("Translation", &translation.x, -1.5f, 1.5f);
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+                ImGui::End();
+            }
+
+            // Rendering
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
             //glDrawArrays(GL_TRIANGLES, 0, 6);
 
             /* Swap front and back buffers */
@@ -125,6 +181,11 @@ int main(void)
             glfwPollEvents();
         }
     }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     glfwTerminate();
     return 0;
